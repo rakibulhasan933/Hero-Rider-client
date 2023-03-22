9b603d3b0cd25a3e313750ee7f0572ec0e83ed57
RAKIBUL <rakibulmd933@gmail.com>
2023-03-23 01:59:12 +0600

    [ADD] password matched

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,7 @@ import Home from "../components/Home/Home";
 import NotFound from "../components/Shared/NotFound";
 import Main from "../Layout/Main";
 import Login from "../components/Auth/Login";
+import Register from "../components/Auth/Register";
 
 
 const router = createBrowserRouter([
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
 			{
 				path: '/login',
 				element: <Login />
+			},
+			{
+				path: '/signup',
+				element: <Register />
 			},
 		]
 	}
