e41ce712151e340f8babf3d7b84c1abe2dd6601e
RAKIBUL <rakibulmd933@gmail.com>
2023-03-22 23:47:56 +0600

    [ADD]home header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import bg from '../../assets/pexels-burak-the-weekender-1253049.jpg';
 
 const Home = () => {
 	return (
-		<div>
-			<h2 className='text-3xl text-center'>Home Pages</h2>
+		<div style={{ background: `url(${bg})` }} className="relative min-h-screen bg-cover ">
+			<div>
+				<div className="absolute bottom-1/3 left-40">
+					<h3 className='my-4 text-xl font-normal text-white'>Life is worth more than time</h3>
+					<button className='p-2 text-base font-medium bg-white rounded w-36 hover:bg-blue-500'><Link to='/pay-bill'>Driving Packages</Link></button>
+				</div>
+			</div>
 		</div>
 	);
 };
