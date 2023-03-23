55098e38371df5f444d08e921e4b9414275dfeee
RAKIBUL <rakibulmd933@gmail.com>
2023-03-23 15:24:46 +0600

    [ADD] porfile

--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -11,7 +11,7 @@ const Profile = () => {
 				setUser(data)
 			});
 	}, [email]);
-	const { _id, name, address, phone, area, CarName, carModel, vehicle, role } = user;
+	const { _id, name, address, phone, area, carName, carModel, vehicle, role } = user;
 	return (
 		<div>
 			<h2 className='text-2xl font-bold text-center'>My Profile </h2>
@@ -37,7 +37,7 @@ const Profile = () => {
 					<h2 className='font-bold '>NID Card Picture</h2>
 					{role === 'student' && <div>
 						<h2 className='font-bold '>Car Name</h2>
-						<h2 className='text-indigo-500 '>{CarName} </h2>
+						<h2 className='text-indigo-500 '>{carName} </h2>
 						<h2 className='font-bold '>Area</h2>
 						<h2 className='text-indigo-500 '>{area}</h2>
 						<h2 className='font-bold '>Driving License Picture</h2>
