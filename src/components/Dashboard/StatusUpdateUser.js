b7f495f9df0139cf14197a641f900e9bb635761f
RAKIBUL <rakibulmd933@gmail.com>
2023-03-23 14:25:20 +0600

    [ADD] blocked confirm

--- src/components/Dashboard/StatusUpdateUser.js
+++ src/components/Dashboard/StatusUpdateUser.js
@@ -5,10 +5,32 @@ import Loading from '../Shared/Loading';
 const StatusUpdateUser = ({ setStatusUpdate, statusUpdate }) => {
 	const [isLoading, setLoading] = useState(false);
 
-	const { name, email } = statusUpdate;
+	const { name, email, blocked } = statusUpdate;
 
 
 	const handleBlock = () => {
+		setLoading(true)
+		fetch(`http://localhost:5000/auth/remove-block/${email}`, {
+			method: 'PATCH',
+			headers: {
+				headers: {
+					'content-type': 'application/json',
+				}
+			}
+		})
+			.then(res => res.json())
+			.then(data => {
+				setLoading(false);
+				if (data.modifiedCount > 0) {
+					toast.success('Rider Blocked successfully');
+					setStatusUpdate(null);
+				}
+				else {
+					toast.error('Failed Blocked Rider')
+				}
+			})
+	}
+	const handleUnBlock = () => {
 		setLoading(true)
 		fetch(`http://localhost:5000/auth/remove-block/${email}`, {
 			method: 'PATCH',
@@ -43,7 +65,7 @@ const StatusUpdateUser = ({ setStatusUpdate, statusUpdate }) => {
 					<h3 className="text-lg font-bold text-blue-600">{email}</h3>
 					<p className="py-4">Are you sure you want to Blocked Rider profile ?</p>
 					<div className="modal-action">
-						<button onClick={() => handleBlock(email)} className="mr-5 btn btn-error">Blocked</button>
+						{blocked === true ? <button onClick={() => handleUnBlock(email)} className="mr-5 btn btn-error">UnBlock</button> : <button onClick={() => handleBlock(email)} className="mr-5 btn btn-error">Blocked</button>}
 						<label htmlFor="status-update-modal" className="justify-center bg-green-700 btn">Cancel</label>
 					</div>
 				</div>
